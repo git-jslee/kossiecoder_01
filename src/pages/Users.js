cd81dc3670fca3e72cf0f2daa5687310382dd3af
jslee <datadam@kakao.com>
2021-08-29 22:16:06 +0900

    로등 spinner 추가

--- src/pages/Users.js
+++ src/pages/Users.js
@@ -1,13 +1,17 @@
 import React, { useState, useEffect } from "react";
 import axios from "axios";
 import UserList from "../components/UserList";
+import Spiner from "../components/Spiner";
 
 function Users() {
   const [users, setUsers] = useState([]);
+  const [loading, setLoading] = useState(true);
+
   useEffect(() => {
     axios.get("https://jsonplaceholder.typicode.com/users").then((response) => {
       console.log(response);
       setUsers(response.data);
+      setLoading(false);
     });
   }, []);
 
@@ -18,7 +22,7 @@ function Users() {
   return (
     <>
       <h1>Users</h1>
-      <UserList users={users} />
+      {loading ? <Spiner /> : <UserList users={users} />}
     </>
   );
 }
