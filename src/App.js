ee14dc6167aaea51401a650583f59f2f90d68d29
jslee <datadam@kakao.com>
2021-08-29 15:28:37 +0900

    MovieForm 컴포넌트-자식컴포넌트에서 부모 컴ㅍ넌트 스테이트 변경

--- src/App.js
+++ src/App.js
@@ -1,52 +1,27 @@
 import React, { useState } from "react";
 import Movie from "./components/Movie";
+import MovieForm from "./components/MovieForm";
 
 function App() {
-  const [movieTitle, setMovieTitle] = useState("");
-  const [movieYear, setMovieYear] = useState("");
   const [movies, setMovies] = useState([
-    { title: "cwcc1", year: 2020 },
-    { title: "cwcc2", year: 2021 },
-    { title: "cwcc3", year: 2022 },
+    { title: "CWCC-1", year: 2020 },
+    { title: "CWCC-2", year: 2021 },
+    { title: "CWCC-3", year: 2022 },
   ]);
 
   const renderMovies = movies.map((movie) => {
     return <Movie movie={movie} key={movie.title} />;
   });
 
-  const addMovie = (event) => {
-    event.preventDefault();
-    setMovies([
-      ...movies,
-      {
-        title: movieTitle,
-        year: movieYear,
-      },
-    ]);
-    setMovieTitle("");
-    setMovieYear("");
+  const addMovie = (movie) => {
+    setMovies([...movies, movie]);
+    console.log(movie);
   };
 
   return (
     <div className="App">
       <h1>Movie list</h1>
-      <form onSubmit={addMovie}>
-        <input
-          type="text"
-          value={movieTitle}
-          placeholder="영화제목"
-          onChange={(e) => setMovieTitle(e.target.value)}
-        />
-        <br />
-        <input
-          type="text"
-          value={movieYear}
-          placeholder="개봉년도"
-          onChange={(e) => setMovieYear(e.target.value)}
-        />
-        <br />
-        <button type="submit">영화추가</button>
-      </form>
+      <MovieForm addMovie={addMovie} />
       {renderMovies}
     </div>
   );
