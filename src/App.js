d8da2f67dcf9205c56b4fd4a9e2c894f6565c8d2
jslee <datadam@kakao.com>
2021-08-29 12:06:50 +0900

    조건렌더링

--- src/App.js
+++ src/App.js
@@ -1,9 +1,20 @@
-import "./App.css";
+import React, { useState, useEffect } from "react";
 
 function App() {
+  const [condition, setCondition] = useState(false);
+  const toggle = () => setCondition(!condition);
+
+  useEffect(() => {
+    console.log(condition);
+  }, [condition]);
+
+  const renderCondition = condition ? "True" : "False";
+
   return (
     <div className="App">
       <h1>테스트</h1>
+      <div>{renderCondition}</div>
+      <button onClick={toggle}>Toggle</button>
     </div>
   );
 }
