4b3007f103c960eeee1e85c93142faea380035ac
jslee <datadam@kakao.com>
2021-08-29 12:24:19 +0900

    반복문

--- src/App.js
+++ src/App.js
@@ -1,20 +1,25 @@
 import React, { useState, useEffect } from "react";
 
 function App() {
-  const [condition, setCondition] = useState(false);
-  const toggle = () => setCondition(!condition);
+  const movies = [
+    { title: "cwcc1", year: 2020 },
+    { title: "cwcc2", year: 2021 },
+    { title: "cwcc3", year: 2022 },
+  ];
 
-  useEffect(() => {
-    console.log(condition);
-  }, [condition]);
-
-  const renderCondition = condition ? "True" : "False";
+  const renderMovies = movies.map((movie) => {
+    return (
+      <div className="movie" key={movie.title}>
+        <div className="movie-title">{movie.title}</div>
+        <div className="movie-year">{movie.year}</div>
+      </div>
+    );
+  });
 
   return (
     <div className="App">
-      <h1>테스트</h1>
-      <div>{renderCondition}</div>
-      <button onClick={toggle}>Toggle</button>
+      <h1>Movie list</h1>
+      {renderMovies}
     </div>
   );
 }
